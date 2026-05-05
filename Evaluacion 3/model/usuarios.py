765011a7d6be4c336626ea363953defdd7d83146
Luis Vicencios <luis.vicencio8@gmail.com>
2026-05-04 22:08:33 -0400

    claves encriptadas

--- Evaluacion 3/model/usuarios.py
+++ Evaluacion 3/model/usuarios.py
@@ -7,18 +7,23 @@ class UsuarioModel:
 
     def crear_usuarios_iniciales(self):
         if self.collection.count_documents({}) == 0:
-            pass_admin = bcrypt.hashpw("123".encode('utf-8'), bcrypt.gensalt())
-            pass_vendedor = bcrypt.hashpw("456".encode('utf-8'), bcrypt.gensalt())
+            admin = bcrypt.hashpw("admin".encode('utf-8'), bcrypt.gensalt())
+            vendedor = bcrypt.hashpw("vendedor".encode('utf-8'), bcrypt.gensalt())
+            
             usuarios_base = [
-                {"username": "admin", "password": "admin", "rol": "administrador"},
-                {"username": "vendedor", "password": "vendedor", "rol": "vendedor"}
+                {"username": "admin", "password": admin, "rol": "administrador"},
+                {"username": "vendedor", "password": vendedor, "rol": "vendedor"}
             ]
             self.collection.insert_many(usuarios_base)
-
     def autenticar(self, username, password):
         usuario = self.collection.find_one({"username": username})
         
         if usuario:
-            if bcrypt.checkpw(password.encode('utf-8'), usuario["password"]):
+            clave_hasheada = usuario["password"]
+            if type(clave_hasheada) == str:
+                clave_hasheada = clave_hasheada.encode('utf-8')
+            
+            if bcrypt.checkpw(password.encode('utf-8'), clave_hasheada):
                 return usuario
+                
         return None
